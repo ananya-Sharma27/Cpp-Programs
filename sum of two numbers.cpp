a35b5264a8cf49ba318d858fb6850e413243c274
ananya-Sharma27 <63068345+ananya-Sharma27@users.noreply.github.com>
2020-08-26 21:54:44 +0530

    Create sum of two numbers.cpp

---
+++ sum of two numbers.cpp
@@ -0,0 +1,13 @@
+//Program to calculate the sum of two numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num1, num2, sum;
+	cout<<"Enter two numbers\n";
+	cin>>num1>>num2;
+	cout<<endl<<"Sum of two numbers is:";
+	sum = num1+num2;
+	cout<<sum;
+	return 0;
+}
