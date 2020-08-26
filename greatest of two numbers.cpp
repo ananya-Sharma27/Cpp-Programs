f56e4443085548c3ba5d4b80d9aaaf6a0cab257b
ananya-Sharma27 <63068345+ananya-Sharma27@users.noreply.github.com>
2020-08-26 21:57:20 +0530

    Update greatest of two numbers.cpp

--- greatest of two numbers.cpp
+++ greatest of two numbers.cpp
@@ -6,8 +6,8 @@ int main()
 	int num1, num2;
 	cout<<"Enter the  first number\n";
 	cin>>num1;
-  cout<<"Enter the second number\n";
-  cin>>num2;
+  	cout<<"Enter the second number\n";
+ 	cin>>num2;
 	if(num1>num2)
 	cout<<"num1 is greater than num2";
 	else
