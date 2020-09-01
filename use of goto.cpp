949a70a63f326649f368647853a28b6212d50d85
ananya-Sharma27 <63068345+ananya-Sharma27@users.noreply.github.com>
2020-09-01 23:30:01 +0530

    Update use of goto.cpp

--- use of goto.cpp
+++ use of goto.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 int main()
 {
-   int num; cout<<"Enter a number: "; cin>>num;
+   int num; cout<<"Enter a number: ";
+   cin>>num;
    if (num % 2==0){
       goto print;
    }
