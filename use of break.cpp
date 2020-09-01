04837b6af393ed2583858eda858ac473ad79b1d3
ananya-Sharma27 <63068345+ananya-Sharma27@users.noreply.github.com>
2020-09-01 23:21:25 +0530

    Create use of break.cpp

---
+++ use of break.cpp
@@ -0,0 +1,14 @@
+//Program for the use of break statement
+#include <iostream>  
+using namespace std;  
+int main() {
+{
+      for (int i = 1; i <= 10; i++)    
+          {    
+              if (i == 5)    
+              {    
+                  break;    
+              }    
+        cout<<i<<"\n";    
+          }    
+}  
