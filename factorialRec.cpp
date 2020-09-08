1a65545fc652d3638de4cd4df432d12ed45d8fd4
ananya-Sharma27 <63068345+ananya-Sharma27@users.noreply.github.com>
2020-09-08 23:05:54 +0530

    Create factorialRec.cpp

---
+++ factorialRec.cpp
@@ -0,0 +1,16 @@
+//program of factorial using recursion
+#include <iostream>
+using namespace std;
+int fact(int n) {
+   if ((n==0)||(n==1))
+   return 1;
+   else
+   return n*fact(n-1);
+}
+int main() {
+   int n;
+   cout<<"Enter the number:";
+   cin>>n;
+   cout<<"Factorial of "<<n<<" is "<<fact(n);
+   return 0;
+}
