6be866ee032c0baff511a9c5806b14df9d80721d
ananya-Sharma27 <63068345+ananya-Sharma27@users.noreply.github.com>
2020-09-14 23:22:22 +0530

    Create pointersAdd.cpp

---
+++ pointersAdd.cpp
@@ -0,0 +1,13 @@
+//program for addition of two numbers in pointers
+#include<iostream>
+using namespace std;
+int main()
+{
+int f,s,*p,*q,sum;
+cout<<"Enter two integers to add\n";
+cin>>f>>s;
+p= &f;
+q=&s;
+sum= *p + *q;
+cout<<"Sum of 2 numbers="<< sum;
+}
