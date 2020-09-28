900f5b2d8822b031031037dcfd8a33665f0f2c2e
ananya-Sharma27 <63068345+ananya-Sharma27@users.noreply.github.com>
2020-09-28 22:53:32 +0530

    Create singleInherit.cpp

---
+++ singleInherit.cpp
@@ -0,0 +1,23 @@
+//program of single inheritance
+#include <iostream>  
+using namespace std;  
+ 
+ class Account 
+ {  
+   public:  
+   float salary = 60000;   
+ };  
+   
+   class Programmer: public Account 
+   {  
+   public:  
+   float bonus = 5000;    
+   };       
+   
+  int main() 
+  {  
+    Programmer p1;  
+     cout<<"Salary: "<<p1.salary<<endl;    
+     cout<<"Bonus: "<<p1.bonus<<endl;    
+     return 0;  
+  }  
